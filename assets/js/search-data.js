7efcbb95ed8fe59908f302c55f16d79e983d8d3b
julieconrads <146424323+julieconrads@users.noreply.github.com>
2026-05-22 18:18:02 +0000

    Deploying to gh-pages from @ julieconrads/julieconrads.github.io@96db6226dfcb26239c2ea530d53a5695ff534c82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,21 +511,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6A%75%6C%69%65.%63%6F%6E%72%61%64%73@%65%61%77%61%67.%63%68", "_blank");
         },
       },{
         id: 'social-rss',
@@ -535,18 +528,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.eawag.ch/de/ueber-uns/portraet/organisation/mitarbeitende/profile/julie-conrads/show/", "_blank");
         },
       },{
       id: 'light-theme',
